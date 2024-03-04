366dd73eacf6feda81a4aba1af4771c617587e52
barsMolecule <bars_molecule@mail.ru>
2024-03-04 17:29:33 +0300

    Версия 0.0.1: После линча 	new file:   .gitignore 	new file:   .vscode/extensions.json 	deleted:    include/README 	new file:   include/pinmap.h 	new file:   include/sensors.h 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,78 @@
 #include "Arduino.h"
 #include "DFilters.h"
-// Потенцометры к A0 A1
-#define POT1_PIN A0
-#define POT2_PIN A1
 
-#define SWITCH_PIN11 3 // Тумблер первый
-#define SWITCH_PIN12 4
+#include "pinmap.h"
 
-#define SWITCH_PIN21 13 // Тумблер второй
-#define SWITCH_PIN22 2
-
-#define CYLINDER_PIN1 -1 // Подключение цилиндры
-#define CYLINDER_PIN2 -1
 
 ERA_filter<float> potFilter(0.1); // Фильтр
 char value;
 // Тумблеры
-bool switchState11 = 0;
+bool _switchState = 0;
 bool switchState12 = 0;
 bool switchState21 = 0;
 bool switchState22 = 0;
 // Тумблеры ласт состояние
-bool lastSwitchState11 = 0;
+
 bool lastSwitchState12 = 0;
 bool lastSwitchState21 = 0;
 bool lastSwitchState22 = 0;
 // Время дребезга
-unsigned long debounceDelay = 50;
+const int DEBOUNCE_DELAY = 50;
 // Время последнего изменения состояния тумблиров
-unsigned long lastDebounceTime11 = 0;
+unsigned long _lastDebounceTime = 0;
 unsigned long lastDebounceTime12 = 0;
 unsigned long lastDebounceTime21 = 0;
 unsigned long lastDebounceTime22 = 0;
 
-void setup()
-{
-
-  Serial.begin(9600);
-
-  // Выводы циллиндров
-  pinMode(CYLINDER_PIN1, OUTPUT);
-  pinMode(CYLINDER_PIN2, OUTPUT);
-
-  // Подтягивающие резисторы на пины
-  pinMode(SWITCH_PIN11, INPUT_PULLUP);
-  pinMode(SWITCH_PIN12, INPUT_PULLUP);
-  pinMode(SWITCH_PIN21, INPUT_PULLUP);
-  pinMode(SWITCH_PIN22, INPUT_PULLUP);
+// Пример функции !!!!!!!!!!!!!!!!!!
+int getAngleHip(){
+  static ERA_filter<float> _era(0.1); // Фильтр
+  return _era.filtered(analogRead(PIN_ANGLE_HIP));
 }
 
-void loop()
-{
-  // чтение значения с потинциометра и фильтрация
-  int potValue1 = potFilter.filtered(analogRead(POT1_PIN));
-  int potValue2 = potFilter.filtered(analogRead(POT2_PIN));
-  // защита от дребезга
-  bool reading11 = !digitalRead(SWITCH_PIN11);
-  bool reading12 = !digitalRead(SWITCH_PIN12);
-  bool reading21 = !digitalRead(SWITCH_PIN21);
-  bool reading22 = !digitalRead(SWITCH_PIN22);
-  if (reading11 != lastSwitchState11)
-  {
-    lastDebounceTime11 = millis();
+
+
+void setup(){
+  Serial.begin(9600); 
+  initPins();
+}
+
+// Функция опроса тумблера
+bool getSwitchState(){
+  bool _reading = !digitalRead(SWITCH_PIN11);
+  static bool _lastSwitchState = 0;
+
+  if (_reading != _lastSwitchState){
+    _lastDebounceTime = millis();
   }
 
-  if (reading12 != lastSwitchState12)
-  {
-    lastDebounceTime12 = millis();
-  }
-
-  if ((millis() - lastDebounceTime11) > debounceDelay)
-  {
-    if (reading11 != switchState11)
-    {
-      switchState11 = reading11;
+  if ((millis() - _lastDebounceTime) > DEBOUNCE_DELAY){
+    if (_reading != _switchState){
+      _switchState = _reading;
     }
   }
 
-  if ((millis() - lastDebounceTime12) > debounceDelay)
+  _lastSwitchState = _reading;
+}
+
+
+void loop(){
+  // чтение значения с потинциометра и фильтрация
+  int potValue1 = potFilter.filtered(analogRead(PIN_ANGLE_HIP));
+  int potValue2 = potFilter.filtered(analogRead(POT2_PIN));
+  // защита от дребезга
+  
+  bool reading12 = !digitalRead(SWITCH_PIN12);
+  bool reading21 = !digitalRead(SWITCH_PIN21);
+  bool reading22 = !digitalRead(SWITCH_PIN22);
+
+  if (reading12 != lastSwitchState12){
+    lastDebounceTime12 = millis();
+  }
+
+  
+
+  if ((millis() - lastDebounceTime12) > DEBOUNCE_DELAY)
   {
     if (reading12 != switchState12)
     {
@@ -94,7 +89,7 @@ if (reading21 != lastSwitchState21) // 2 тумблер
     lastDebounceTime22 = millis();
   }
 
-  if ((millis() - lastDebounceTime21) > debounceDelay)
+  if ((millis() - lastDebounceTime21) > DEBOUNCE_DELAY)
   {
     if (reading21 != switchState21)
     {
@@ -102,24 +97,24 @@ if (reading21 != lastSwitchState21) // 2 тумблер
     }
   }
 
-  if ((millis() - lastDebounceTime22) > debounceDelay)
+  if ((millis() - lastDebounceTime22) > DEBOUNCE_DELAY)
   {
     if (reading22 != switchState22)
     {
       switchState22 = reading22;
     }
   }
-  lastSwitchState11 = reading11;
+
   lastSwitchState12 = reading12;
   lastSwitchState21 = reading21;
   lastSwitchState22 = reading22;
   // Включение циллиндров от тумблеров
-  digitalWrite(CYLINDER_PIN1, switchState11);
+  digitalWrite(CYLINDER_PIN1, _switchState);
   digitalWrite(CYLINDER_PIN2, switchState12);
 
   // Вывод значения фильтрованого в порт
 
-  Serial.print(switchState11);
+  Serial.print(_switchState);
   Serial.print(",");
   Serial.print(switchState12);
   Serial.print(",");
