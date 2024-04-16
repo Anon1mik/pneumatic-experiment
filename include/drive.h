e969958282eff02df37e4b6ee9a0c69badd7cd3f
Anon1mik <gaymany97@gmail.com>
2024-04-16 13:40:56 +0300

    0.1.4

--- include/drive.h
+++ include/drive.h
@@ -6,6 +6,7 @@ void regulatorHip(int deathZone)
 
     static int legError; // Переменная для расчёта ошибки
 
+    // Использование полинома
     legError = polinom3(1000, 0, 900);
 
     const int _deathZone = deathZone / 2; // Мёртвая зона
@@ -19,4 +20,26 @@ void regulatorHip(int deathZone)
         else if (legError < _downDeathZone) LegUP();
         else                                LegSTOP();
     }
+}
+// Ручное управление, если кнопка не нажата
+void pneumaticCulyndr()
+{
+  // Управление цилиндром левой ноги в зависимости от положения левого тумблера
+  if (buttonState == 0)
+  {
+    if (getTumblerLeft() == 2)
+      LegUP();
+    else if (getTumblerLeft() == 1)
+      LegDOWN();
+    else
+      LegSTOP();
+
+    // Управление цилиндром правого колена в зависимости от положения правого тумблера
+    if (getTumlerRight() == 2)
+      KneeUP();
+    else if (getTumlerRight() == 1)
+      KneeDOWN();
+    else
+      KneeSTOP();
+  }
 }
